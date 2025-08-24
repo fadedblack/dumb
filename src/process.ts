e3c0045f6daa8e1a7b4e957d1f327aff4f30825e
fadedBlack <inkeetcha234@gmail.com>
2025-08-24 12:28:33 +0530

    [Inkeet] Integrates llm with processing prompt

--- src/process.ts
+++ src/process.ts
@@ -1,12 +1,15 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
 import { enrichUserPrompt, generateEnvConfig } from "./enrichUserPrompt.ts";
+import { interpretPrompt } from "./interpretPrompt.ts";
+import { getConfig } from "./config.ts";
 
 export const process = (args: string[]): void => {
   try {
     const argString = checkArgs(args);
     console.log("Arguments:", argString);
 
-    const config = generateEnvConfig();
-    enrichUserPrompt(argString, config);
+    const prompt = enrichUserPrompt(argString, generateEnvConfig());
+    interpretPrompt(prompt, GoogleGenerativeAI, getConfig).then(console.log);
   } catch (error) {
     console.error(error);
   }
